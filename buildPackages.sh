928540dc92330ba1f31a34b39f4d63a3b87f7a1b
pr414 <paoloriveng@gmail.com>
2026-02-20 17:20:05 +0100

    Humble early porting

--- buildPackages.sh
+++ buildPackages.sh
@@ -1,8 +1,22 @@
-export ROS2_DISTRO=jazzy
-# Update and install required packages
-apt update && apt install -y \
+export ROS2_DISTRO=humble
+# Update and install required packages for jazzy
+#apt update && apt install -y \
+#    ros-${ROS2_DISTRO}-navigation2 \
+#    ros-${ROS2_DISTRO}-nav2-* \
+#    ros-${ROS2_DISTRO}-ros-gz \
+#    ros-${ROS2_DISTRO}-tf2-geometry-msgs \
+#    ros-${ROS2_DISTRO}-joint-state-publisher \
+#    ros-${ROS2_DISTRO}-pointcloud-to-laserscan \
+#    ros-${ROS2_DISTRO}-tf-transformations \
+#    ros-${ROS2_DISTRO}-joint-state-publisher-gui \
+#    ros-${ROS2_DISTRO}-joint-state-broadcaster \
+#    ros-${ROS2_DISTRO}-rmw-cyclonedds-cpp \
+#    xterm
+
+# Update and install required packages for humble
+apt install -y \
     ros-${ROS2_DISTRO}-navigation2 \
-    ros-${ROS2_DISTRO}-nav2-* \
+    ros-${ROS2_DISTRO}-nav2-bringup \
     ros-${ROS2_DISTRO}-ros-gz \
     ros-${ROS2_DISTRO}-tf2-geometry-msgs \
     ros-${ROS2_DISTRO}-joint-state-publisher \
